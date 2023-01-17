af4527025d39ea045670d27fb3d113088f9f5cc1
Prachi Bhatt <104725245+PrachiBhatt443@users.noreply.github.com>
2023-01-17 20:52:27 +0530

    Add files via upload

---
+++ alphapyramid.py
@@ -0,0 +1,20 @@
+for i in range(1,5+1):
+    for k in range(0,i):
+        print(chr(64+i),end="")
+    print()
+print("____________________________,\n")
+for i in range(1,5+1):
+    for k in range(0,i):
+        print(chr(64+k),end="")
+    print()
+print("____________________________,\n")
+for i in range(1,5+1):
+    for k in range(5+1,i,-1):
+        print(chr(64+i),end="")
+    print()
+print("____________________________,\n")
+for i in range(1,5+1):
+    for k in range(5+1,i,-1):
+        print(chr(64+k),end="")
+    print()
+print("____________________________,\n")
