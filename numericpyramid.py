11ee3056ae8985acd12955d7728a537c030087be
Prachi Bhatt <104725245+PrachiBhatt443@users.noreply.github.com>
2023-01-22 22:59:54 +0530

    Update numericpyramid.py

--- numericpyramid.py
+++ numericpyramid.py
@@ -25,3 +25,13 @@ for row in range(1, rows):
         print(column, end=’ ‘)
     print(“”)
 print("____________________________\n")
+rows = 5
+b = 0
+for i in range(rows, 0, -1):
+    b += 1
+    for j in range(1, i + 1):
+        print(b, end=’ ‘)
+    print(‘\r’)
+print("____________________________\n")
+
+
